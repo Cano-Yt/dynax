9967bdc14e5c94390778ae7c641e02d44a29b606
Glitch (hello-webpage) <none>
2021-02-13 17:43:58 +0000

    🏗🐽 Checkpoint ./komutlar/bakım.js:28467901/1163

--- komutlar/bakım.js
+++ komutlar/bakım.js
@@ -4,13 +4,32 @@ const db = require("quick.db")
 
 
 exports.run = async(client, message, args) => {
-      let dil = db.fetch(`sunucudili_${message.guild.id}`)
-      if(dil == "TR"){
-      if (message.author.id !== "351008627726876692" || message.author.id !== "786240484879237147") return message.channel.send(`Sahip olda gel koçum`)
-      }
-  if(dil == "EN"){
-    if (message.author.id !== "351008627726876692" || message.author.id !== "786240484879237147") return message.channel.send(`For this command you must have Owner bot`)
+if (message.author.id !== "351008627726876692" || message.author.id !== "786240484879237147") return message.channel.send(`Sahip olda gel koçum | For this command you must have Owner bot`)
+message.channel.send(`Bakım Al/çıkar`)
+  let bakım = db.fetch(`bakım`)
+  if(bakım !== "bakımda") {
+  if(args[0] == "al") {
+  db.set(`bakım`, "bakımda")
+    message.channel.send(`Botu bakıma aldım.`)
   }
-  
-      
+    if(bakım == "bakımda") {
+      if(args[0] == "çıkar"){
+        db.set(`bakım`, "bakımda-değil")
+        message.channel.send(`Botu bakımdan çıkardım artık kullanılabilir.`)
       }
+    }
+}
+      }
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'bakım',
+    description: '',
+    usage: ''
+  };
