ffd2fa6aa8f71d7fd363a4998cdcf519872a79e2
Glitch (hello-webpage) <none>
2021-02-13 16:33:53 +0000

    💓💫 Checkpoint ./komutlar/öp.js:28467901/388

--- komutlar/öp.js
+++ komutlar/öp.js
@@ -2,26 +2,23 @@ const Discord = require('discord.js');
 
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-	          const ayarlar = require('../ayarlar.json')
-				    let prefix = await require('quick.db').fetch(`prefix.${message.guild.id}`) || ayarlar.prefix
+const ayarlar = require('../ayarlar.json')
+let prefix = await require('quick.db').fetch(`prefix.${message.guild.id}`) || ayarlar.prefix
 
-    
-  try {
+  
     let member = message.mentions.members.first();
 
     require('request')({url: 'https://nekos.life/api/kiss', json: true}, (req, res, json) => {
       if (member) {
+        if(!member) return message.reply('Öpmek istediğin kullanıcıyı etiketlemelisin!');
+        if(member == message.author) return message.channel.send(Ke)
         let embed = new Discord.MessageEmbed()
         .setTitle(message.author.username +" " + member.user.username+ ' Adlı kullanıcıyı Öpüyor!')
         .setColor('#363942')
         .setImage(json.url);
-
         message.channel.send(embed);
-      } else message.reply('Öpmek istediğin kullanıcıyı etiketlemelisin!');
+      }
     });
-  } catch (err) {
-    message.channel.send('Hata!\n' + err).catch();
-  }
 
 };
 
