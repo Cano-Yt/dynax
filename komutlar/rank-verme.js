9dd6178fd9dabaef58732a01184c011ed35ec096
Glitch (hello-webpage) <none>
2021-02-15 06:31:04 +0000

    🎒🖼 Checkpoint ./komutlar/rank-verme.js:28467901/412

--- komutlar/rank-verme.js
+++ komutlar/rank-verme.js
@@ -1,3 +1,17 @@
+const ayarlar = require("../ayarlar.json")
+const Discord = require("discord.js");
+const db = require("quick.db")
 
 
+exports.run = async(client, message, args) => {
 if (message.author.id !== "351008627726876692" && message.author.id !== "786240484879237147") return message.channel.send(`Sahip olda gel koçum | For this command you must have Owner of the bot`)
+  /*
+Ranklar
+sahip
+vip
+altın üye
+*/
+  if(message !== "sahip" || "vip" || "altın üye") {
+    message.channel.send(`Sahipim Lütfen `)
+  }
+}
