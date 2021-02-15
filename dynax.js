760654a63d1a5a6b9289589f55845c1af2507032
Glitch (hello-webpage) <none>
2021-02-15 12:21:09 +0000

    👕💆 Checkpoint ./dynax.js:28467901/134

--- dynax.js
+++ dynax.js
@@ -78,27 +78,30 @@ client.on("guildMemberAdd", async member => {
 let kanal = db.fetch(`otorolkanal_${member.guild.id}`)
 let rol = db.fetch(`otoRol_${member.guild.id}`)
 let dil = db.fetch(`dil_${member.guild.id}`)
-let rol2 = member.guild.roles.cache.find(c => c.id === `${rol}`)
 if(!rol) return;
 member.roles.add(rol)
 
 if(dil == "TR") {
 const embed = new Discord.MessageEmbed()
+
+
 .setTitle(`${client.user.username} - Otorol sitemi`)
 .setDescription(`
 Sunucuya ${member.user.tag} Adlı üye katıldı. Hoş geldin.
-${rol2} Adlı rol üyeye verildi.
+<@${rol}> Adlı rol üyeye verildi.
 Senin gelmenle ${member.guild.memberCount} Kişi olduk!`)
-member.guild.channels.cache.get(kanal).send(embed)
+return member.guild.channels.cache.get(kanal).send(embed) 
 }
 if(dil == "EN") {
 const embed1 = new Discord.MessageEmbed()
+
+
 .setTitle(`${client.user.username} - Otorol sitemi`)
 .setDescription(`
 ${member.user.tag} Named member joined the server. Welcome.
-${rol2} Named role was give a role.
+<@${rol}> Named role was give a role.
 With your join we have total ${member.guild.memberCount} Person!`)
-member.guild.channels.cache.get(kanal).send(embed1)
+return member.guild.channels.cache.get(kanal).send(embed1)
 }    
 })
 
