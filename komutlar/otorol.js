aad88e74515fc32f9eedc35e8e9202e95270d9f2
Glitch (hello-webpage) <none>
2021-02-15 08:54:10 +0000

    🍼🏏 Checkpoint ./komutlar/otorol.js:28467901/1724

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -12,15 +12,33 @@ let bakım = db.fetch(`bakım`)
     if(dil=="EN") return message.channel.send(`Bot repairing!\nPlease come support server and learn the problem. To come ${prefix}invite`)
   } else {
     let kanal = message.mentions.channels.first()
-    let rol = message.mentions.channels.first()
+    let rol = message.mentions.roles.first()
     if(dil == "TR") {
       if(!rol) return message.channel.send(`Otomatik olarak verilecek rolü belirtmelisin`)
       if(!kanal) {
         db.set(`otoRol_${message.guild.id}`, rol.id)
         message.channel.send(`Bir kanal etiketlemediğiniz için Sadece otomatik rol vericektir. mesaj atmıyacaktır.`)
+      } else {
+        db.set(`otoRol_${message.guild.id}`, rol.id)
+        db.set(`otorolkanal_${message.guild.id}`, kanal.id)
+        message.channel.send(`Otomatik rol verme ve mesaj atma sistemi açılmıştır.`)
       }
     }
+   if(dil == "EN") {
+         let kanal = message.mentions.channels.first()
+    let rol = message.mentions.roles.first()
+      if(!rol) return message.channel.send(`You must tag the role to be set automatically`)
+      if(!kanal) {
+        db.set(`otoRol_${message.guild.id}`, rol.id)
+        message.channel.send(`Don't sent message. Because you do not tag a channel, it is only an automatic give role system has set.`)
+      } else {
+        db.set(`otoRol_${message.guild.id}`, rol.id)
+        db.set(`otorolkanal_${message.guild.id}`, kanal.id)
+        message.channel.send(`Automatic role give and message sent system has set.`)
+      }
+   
   }
+}
 }
 exports.conf = {
     enabled: true,
