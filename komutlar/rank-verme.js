309171383d1ebf1a550e7be93116b211fa4e18c7
Glitch (hello-webpage) <none>
2021-02-15 06:19:04 +0000

    👙🖇 Checkpoint ./komutlar/rank-verme.js:28467901/197

---
+++ komutlar/rank-verme.js
@@ -0,0 +1,3 @@
+
+
+if (message.author.id !== "351008627726876692" && message.author.id !== "786240484879237147") return message.channel.send(`Sahip olda gel koçum | For this command you must have Owner of the bot`)
