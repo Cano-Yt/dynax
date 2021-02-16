083c8364490e032222f2809e440d70296bbe420f
Glitch (hello-webpage) <none>
2021-02-16 03:11:54 +0000

    🐈🍀 Checkpoint ./komutlar/ayarlar.js:28467901/1593 ./komutlar/sayaç.js:28467901/220

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -11,8 +11,24 @@ if(bakım == "bakımda") {
  if(dil=="TR") return message.channel.send(`Bot bakımda!\nLütfen destek sunucumuza gelerek sorunu öğreniniz. Gelmel için ${prefix}davet`)
  if(dil=="EN") return message.channel.send(`Bot repairing!\nPlease come support server and learn the problem. To come ${prefix}invite`)
 } else {
-  
-  
+  //--------dataları fetchleyelim---------//
+    let otorolaktif = await db.fetch(`otoRol_${message.guild.id}`)    // otorol rol'ü / aktif olup olmadığı
+    let otokanal = await db.fetch(`otorolkanal_${message.guild.id}`)  //Otorol kanalı
+    let sayackanal = await db.fetch(`sayackanal_${message.guild.id}`) //sayaç kanalı
+    let sayacsayı =  await db.fetch(`sayacsayı_${message.guild.id}`)  //sayaç sayısı
+    
+    //Sunucuda fetchlediğimiz şeyleri bulalım//
+    let otorolRolü = message.guild.roles.cache.find(c => c.id === otorolaktif)
+    let otorolkanalı = message.guild.channels.cache.find(c => c.id === otokanal)
+    //Mükemmel bir embed tasarımı yapalım//
+if(dil == "TR") {
+const embed = new Discord.MessageEmbed()
+  .setTitle(`${message.guild.name} - Sunucu ayarları`)
+.addField(`Otorol Rolü`, otorolRolü, true)
+.addField(`Otorol kanalı`, otorolkanalı, true)
+
+message.channel.send(embed)
+}
   
   
 }
@@ -20,12 +36,12 @@ if(bakım == "bakımda") {
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ["repair"],
+    aliases: ["settings"],
     permLevel: 0
   };
   
   exports.help = {
-    name: 'bakım',
+    name: 'ayarlar',
     description: '',
     usage: ''
   };
