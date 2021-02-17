364a05f5c936ae6040095fad52e37903b22e1e80
Glitch (overjoyed-mandrill-s) <none>
2021-02-17 11:33:35 +0000

    ☔️🛎 Checkpoint ./dynax.js:28467901/2 ./komutlar/isimdegis.js:28467901/6561

---
+++ komutlar/isimdegis.js
@@ -0,0 +1,38 @@
+const ayarlar = require("../ayarlar.json")
+const Discord = require("discord.js");
+const db = require("quick.db")
+
+
+exports.run = async(client, message, args) => {
+    let dil = db.fetch(`sunucudili_${message.guild.id}`)
+    let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix    
+      let bakım = db.fetch(`bakım`) 
+      if(bakım == "bakımda") {
+            if(dil=="TR") return message.channel.send(`Bot bakımda!\nLütfen destek sunucumuza gelerek sorunu öğreniniz. Gelmel için ${prefix}davet`)
+    if(dil=="EN") return message.channel.send(`Bot repairing!\nPlease come support server and learn the problem. To come ${prefix}invite`)
+  } else {
+    let member = message.mentions.members.first()
+    let isim;
+      if(args[0]) {
+   isim = args.slice(0).join(' '); 
+} else {
+  if(dil = "TR") {return message.channel.send(`Lütfen bir yazı veya sayı yazınız.`)}
+  if(dil = "EN") {return message.channel.send(`Please write a text or a number.`)}
+}
+    member.setNickname(`${isim}`)
+    if(dil == "TR") {return message.channel.send(`${member} Adlı kullanıcının ismi \`${isim}\` Olarak değiştirildi`)}
+    if(dil == "EN") {return message.channel.send(`${member} Named user nickname set \`${isim}\` Olarak değiştirildi`)}
+  }
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'deneme',
+    description: '',
+    usage: ''
+  };
