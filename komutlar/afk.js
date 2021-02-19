33d9546696a9b2f2b79aeee61fb73b23061959f2
Glitch (overjoyed-mandrill-s) <none>
2021-02-19 08:36:38 +0000

    🖇🌞 Checkpoint ./komutlar/afk.js:28467901/2651

--- komutlar/afk.js
+++ komutlar/afk.js
@@ -9,47 +9,27 @@ exports.run = async (client, message, args) => {
   if(afk) return;
   let prefix = ayarlar.prefix
   let sebeb = args.slice(0).join(' ');
+  
+ let atılma;
+  if(dil == "TR") {
+  moment.locale('tr') 
   let atılmaay = moment(Date.now()+10800000).format("MM")
   let atılmagün = moment(Date.now()+10800000).format("DD")
   let atılmasaat = moment(Date.now()+10800000).format("HH:mm:ss")
-  let atılma = `\`${atılmagün} ${atılmaay.replace(/01/, 'Ocak').replace(/02/, 'Şubat').replace(/03/, 'Mart').replace(/04/, 'Nisan').replace(/05/, 'Mayıs').replace(/06/, 'Haziran').replace(/07/, 'Temmuz').replace(/08/, 'Ağustos').replace(/09/, 'Eylül').replace(/10/, 'Ekim').replace(/11/, 'Kasım').replace(/12/, 'Aralık')} ${atılmasaat}\``
-  let atılmaen = `\`${atılmagün} ${atılmaay.replace(/01/, 'Ocak').replace(/02/, 'Şubat').replace(/03/, 'Mart').replace(/04/, 'Nisan').replace(/05/, 'Mayıs').replace(/06/, 'Haziran').replace(/07/, 'Temmuz').replace(/08/, 'Ağustos').replace(/09/, 'Eylül').replace(/10/, 'Ekim').replace(/11/, 'Kasım').replace(/12/, 'Aralık')} ${atılmasaat}\``
-  
-  
-  moment.locale('tr');
-  
-  if(!sebeb) {
-    if(dil == "TR") {
-  await db.set(`afksebeb_${message.author.id}_${message.guild.id}`, "Belirtilmemiş.")
-  await db.set(`afkoldu_${message.author.id}_${message.guild.id}`, "evet")
-  await db.set(`giriş_${message.author.id}_${message.guild.id}`, atılma)
-    const embed = new Discord.MessageEmbed()
-    .setTitle(`AFK sistemi`)
-    .setDescription(`
-    **${message.author.tag} Afk oldu**
-    **Sebebi : **\`Belirtilmemiş.\`
-    **Giriş zamanı: **${atılma}
-    `)
-    .setThumbnail("https://cdn.discordapp.com/attachments/783001757596254238/811956462666514442/811586592102940704.png")
-    message.channel.send(embed)
-  } else {
-    await db.set(`afksebeb_${message.author.id}_${message.guild.id}`, sebeb)
-    await db.set(`afkoldu_${message.author.id}_${message.guild.id}`, "evet")
-    await db.set(`giriş_${message.author.id}_${message.guild.id}`, atılma)
-    const embed = new Discord.MessageEmbed()
-    .setTitle(`AFK sistemi`)
-    .setDescription(`
-    **${message.author.tag} Afk oldu**
-    **Sebebi : **\`${sebeb}\`
-    **Giriş zamanı: **${atılma}
-    `)
-    .setThumbnail("https://cdn.discordapp.com/attachments/783001757596254238/811956462666514442/811586592102940704.png")
-    message.channel.send(embed)
+ atılma = `\`${atılmagün} ${atılmaay.replace(/01/, 'Ocak').replace(/02/, 'Şubat').replace(/03/, 'Mart').replace(/04/, 'Nisan').replace(/05/, 'Mayıs').replace(/06/, 'Haziran').replace(/07/, 'Temmuz').replace(/08/, 'Ağustos').replace(/09/, 'Eylül').replace(/10/, 'Ekim').replace(/11/, 'Kasım').replace(/12/, 'Aralık')} ${atılmasaat}\``
   }
-  }
+  
   if(dil == "EN") {
-    if(!sebeb) {
+    moment.locate("en")
+  let atılmaay = moment(Date.now()+10800000).format("MM")
+  let atılmagün = moment(Date.now()+10800000).format("DD")
+  let atılmasaat = moment(Date.now()+10800000).format("HH:mm:ss")
+  let atılmaen = `\`${atılmagün} ${atılmaay.replace(/01/, 'January').replace(/02/, 'February').replace(/03/, 'March').replace(/04/, 'April').replace(/05/, 'May').replace(/06/, 'June').replace(/07/, 'July').replace(/08/, 'August').replace(/09/, 'September').replace(/10/, 'October').replace(/11/, 'November').replace(/12/, 'December')} ${atılmasaat}\``
+  } 
+  
+
     if(dil == "TR") {
+  if(!sebeb) {
   await db.set(`afksebeb_${message.author.id}_${message.guild.id}`, "Belirtilmemiş.")
   await db.set(`afkoldu_${message.author.id}_${message.guild.id}`, "evet")
   await db.set(`giriş_${message.author.id}_${message.guild.id}`, atılma)
@@ -75,6 +55,36 @@ exports.run = async (client, message, args) => {
     `)
     .setThumbnail("https://cdn.discordapp.com/attachments/783001757596254238/811956462666514442/811586592102940704.png")
     message.channel.send(embed)
+  }
+  }
+ if(dil == "EN") {
+    if(!sebeb) {
+  await db.set(`afksebeb_${message.author.id}_${message.guild.id}`, "Not specified.")
+  await db.set(`afkoldu_${message.author.id}_${message.guild.id}`, "evet")
+  await db.set(`giriş_${message.author.id}_${message.guild.id}`, atılma)
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`AFK system`)
+    .setDescription(`
+    **${message.author.tag} is afk**
+    **Reason: ** \`Not specified.\`
+    **Giriş zamanı: **${atılma}
+    `)
+    .setThumbnail("https://cdn.discordapp.com/attachments/783001757596254238/811956462666514442/811586592102940704.png")
+    message.channel.send(embed)
+  } else {
+    await db.set(`afksebeb_${message.author.id}_${message.guild.id}`, sebeb)
+    await db.set(`afkoldu_${message.author.id}_${message.guild.id}`, "evet")
+    await db.set(`giriş_${message.author.id}_${message.guild.id}`, atılma)
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`AFK system`)
+    .setDescription(`
+    **${message.author.tag} is afk**
+    **Reason : **\`${sebeb}\`
+    **Entry time: **${atılmaen}
+    `)
+    .setThumbnail("https://cdn.discordapp.com/attachments/783001757596254238/811956462666514442/811586592102940704.png")
+    message.channel.send(embed)
+  }
   }
 
 };
